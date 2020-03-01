e0ae725e85b30896d872825960a14019f1069ce9
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-03-01 14:02:48 +0100

    Den henter nu "tilfældig" titel nu, og printer den

--- Kode/Testetest.py
+++ Kode/Testetest.py
@@ -1,26 +1,23 @@
 import json
 import requests
+import random
 
 headers = {
     "Accept": "application/vnd.api+json",
     "Content-Type": "application/vnd.api+json"
 }
 
-print("\n\n")
+theNum = random.randrange(1,9999)
+print("\nID = "+str(theNum))
 
-response = requests.get("http://kitsu.io/api/edge/anime?filter%5Bid%5D=11001&fields%5Banime%5D=canonicalTitle", headers=headers) 
+response = requests.get("http://kitsu.io/api/edge/anime?filter%5Bid%5D="+str(theNum)+"&fields%5Banime%5D=canonicalTitle", headers=headers)
 
 if response.status_code == 200:
-    print("meme")
+    anime = response.json()
+    info = anime.get("data")
+    fuck = info[0]
+    titel = fuck.get("attributes")
+    print(titel.get("canonicalTitle"))
+
 else:
     print("Failure!", response.status_code)
-
-
-anime = response.json()
-# info = anime.get("data")
-# fuck = info[0]
-
-
-
-
-
