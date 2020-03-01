46ba102e937258478bf21e582b44c9142477923d
Chongsenpai <simonjulendal1201@gmail.com>
2020-03-01 17:16:39 +0100

    gamer UwU

--- Kode/Testetest.py
+++ Kode/Testetest.py
@@ -23,8 +23,13 @@ else:
     print("Failure!", response.status_code)
 
 txt = titel.get("canonicalTitle")
+#^^ gemmer en random title i en vaiabel.
 x = txt.split()
+#^^ tager stringen fra txt og laver det om til en liste, hvor hvert ord er en item i listen, derefter gemmer den listen i en vaiabel.
 answer = input("Choose a word that will replace a random word in the title above UwU\n")
+#^^ gemmer et input i en variabel, som vi skal bruge til at bytte ud med et tilfældigt ord fra anime titlen vi fik.
 result = txt.replace(random.choice(x), answer)
+#^^ koden tager et tilfældigt ord fra listen x, og bytter det ud med ordet "answer" og gemmer det i en vaiabel.
 
 print(result)
+#^^ printer resultatet af ordenes udbytning.
