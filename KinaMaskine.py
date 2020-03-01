c00c819945da316ee9feb6246d7e4b390909db90
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-03-01 20:36:07 +0100

    gjorde rent

--- KinaMaskine.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api = requests.get('https://www.cardmarket.com/en/Magic')
-
-if api.status_code == 200:
-    #Hvis siden er oppe, definerer den det som text i en variabel
-    t = api.text
-    #Så bliver teksten printet
-    print(t)
